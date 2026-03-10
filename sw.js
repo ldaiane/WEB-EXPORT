6569b5201c4b4746fd488662b02d56349229ecf5
ldaiane <ldaianestefanie@gmail.com>
2026-03-10 00:52:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,44 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = 'v1_web_export_cache';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './estilo.css',
+  './app.js',
+  './manifest.json'
+];
 
-self.skipWaiting()
+// Instalação: Cacheia os arquivos essenciais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Arquivos em cache com sucesso!');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
 
-})
+// Ativação: Limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Busca: Serve os arquivos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
